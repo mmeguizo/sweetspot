058939bda0a6647c037d9fb7f83ddc5b23283719
mmeguizo <mark.meguizo@gmail.com>
2024-03-28 13:09:23 +0800

    testser

--- config/database.js
+++ config/database.js
@@ -7,6 +7,6 @@ const hash = crypto
 
 module.exports = {
   // uri: process.env.DB_URL,
-  uri: process.env.DB_LOCAL,
+  uri: "mongodb+srv://meguizo:mongolDB@cluster0.zwkop.mongodb.net/suitespot?retryWrites=true&w=majority",
   secret: hash,
 };
